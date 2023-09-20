f20b865074cf3b403fcfcb9233475791274ceb65
Carlos - Locaweb <carlos@pavaotech.com.br>
2023-09-20 03:12:49 -0300

    Adicionando os tipos de variaveis

---
+++ CursoCompleto/src/edu/carlos/TiposDeVariáveis/TipoVariaveis.java
@@ -0,0 +1,19 @@
+package edu.carlos.TiposDeVariáveis;
+
+public class TipoVariaveis {
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        
+        numero = 2;
+
+        final int ValorNuncaPoderaAlterar = 10;
+
+        
+    }
+}
