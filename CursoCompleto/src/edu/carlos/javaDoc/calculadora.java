d013fc0666661d8bda06fc1d48b3f2fae05c04d1
Carlos - Locaweb <carlos@pavaotech.com.br>
2023-09-21 05:50:41 -0300

    desafio 1

--- CursoCompleto/src/edu/carlos/javaDoc/calculadora.java
+++ CursoCompleto/src/edu/carlos/javaDoc/calculadora.java
@@ -26,5 +26,12 @@ public class Calculadora {
         return numeroUm - numeroDois;
     }
 }
+/**
+Criando nossa documentação no formato html, para disponibilizar via web.
+
+// No terminal execute o comando abaixo
+
+javadoc -encoding UTF-8 -docencoding ISO-8859-1  -d ../docs  src/*.java
+  */
 
 }
